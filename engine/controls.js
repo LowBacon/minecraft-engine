3c49097f01ec12019541079281147e897c6ef961
LowBacon <lenni.a.jokitalo@gmail.com>
2026-06-08 14:39:41 +0300

    Update controls.js

--- engine/controls.js
+++ engine/controls.js
@@ -1,19 +1,22 @@
-export function setupControls(camera){
+import { yaw } from "./player.js";
 
-    let yaw=0, pitch=0;
+export function setupControls(camera) {
+
+    let pitch = 0;
 
     const canvas = document.getElementById("game");
 
-    canvas.onclick = ()=>canvas.requestPointerLock();
+    canvas.onclick = () => canvas.requestPointerLock();
 
-    document.addEventListener("mousemove",e=>{
-        if(document.pointerLockElement!==canvas) return;
+    document.addEventListener("mousemove", e => {
+        if (document.pointerLockElement !== canvas) return;
 
-        yaw -= e.movementX*0.002;
-        pitch -= e.movementY*0.002;
+        // ✅ THIS updates SAME yaw
+        yaw -= e.movementX * 0.002;
+        pitch -= e.movementY * 0.002;
 
-        pitch=Math.max(-1.5,Math.min(1.5,pitch));
+        pitch = Math.max(-1.5, Math.min(1.5, pitch));
 
-        camera.rotation.set(pitch,yaw,0,"YXZ");
+        camera.rotation.set(pitch, yaw, 0, "YXZ");
     });
 }
