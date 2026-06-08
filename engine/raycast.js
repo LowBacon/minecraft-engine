15b1c5e73e0b63947797a04a1d933485266d1039
LowBacon <lenni.a.jokitalo@gmail.com>
2026-06-08 14:25:37 +0300

    Update raycast.js

--- engine/raycast.js
+++ engine/raycast.js
@@ -1,12 +1,13 @@
+import * as THREE from "https://unpkg.com/three@0.152.2/build/three.module.js";
 import { getSelectedBlock } from "./inventory.js";
 
-const raycaster=new THREE.Raycaster();
+const raycaster = new THREE.Raycaster();
 
 export function handleMouse(camera){
 
-  window.addEventListener("mousedown",e=>{
+  window.addEventListener("mousedown", e => {
 
-    raycaster.setFromCamera(new THREE.Vector2(0,0),camera);
+    raycaster.setFromCamera(new THREE.Vector2(0,0), camera);
 
     // simplified (expand later)
     console.log("Clicked block");
